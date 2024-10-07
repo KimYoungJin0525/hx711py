14dfd62b8f1f26b8e6d815f6781146ecec3a61ea
KimYoungJin525 <kimdudwls15951@gmail,com>
2024-10-07 21:55:40 +0900

    feat : test가 필요한 부분

--- example.py
+++ example.py
@@ -5,74 +5,48 @@ from hx711 import HX711
 
 def cleanAndExit():
     print("Cleaning...")
-        
+    GPIO.cleanup()  # GPIO 핀 해제
     print("Bye!")
     sys.exit()
 
-hx = HX711(5, 6)
+# 첫 번째 HX711 - 엑셀(Accelerator)
+hx1 = HX711(5, 6)  # 첫 번째 HX711의 데이터 핀과 클럭 핀
 
-'''
-I've found out that, for some reason, the order of the bytes is not always the same between versions of python,
-and the hx711 itself. I still need to figure out why.
+# 두 번째 HX711 - 브레이크(Brake) 첫번째 완성후 넘어갈것
+# hx2 = HX711(13, 19)  # 두 번째 HX711의 데이터 핀과 클럭 핀
 
-If you're experiencing super random values, change these values to MSB or LSB until you get more stable values.
-There is some code below to debug and log the order of the bits and the bytes.
+# MSB 순서로 설정
+hx1.set_reading_format("MSB", "MSB")
+# hx2.set_reading_format("MSB", "MSB") 첫번째 완성후 넘어갈것
 
-The first parameter is the order in which the bytes are used to build the "long" value. The second paramter is
-the order of the bits inside each byte. According to the HX711 Datasheet, the second parameter is MSB so you
-shouldn't need to modify it.
-'''
-hx.set_reading_format("MSB", "MSB")
-
-'''
-# HOW TO CALCULATE THE REFFERENCE UNIT
-1. Set the reference unit to 1 and make sure the offset value is set.
-2. Load you sensor with 1kg or with anything you know exactly how much it weights.
-3. Write down the 'long' value you're getting. Make sure you're getting somewhat consistent values.
-    - This values might be in the order of millions, varying by hundreds or thousands and it's ok.
-4. To get the wright in grams, calculate the reference unit using the following formula:
-        
-    referenceUnit = longValueWithOffset / 1000
-        
-In my case, the longValueWithOffset was around 114000 so my reference unit is 114,
-because if I used the 114000, I'd be getting milligrams instead of grams.
-'''
-
+# 참조 단위 설정 (로드셀 보정값)
 referenceUnit = 114
-hx.set_reference_unit(referenceUnit)
+hx1.set_reference_unit(referenceUnit)
+# hx2.set_reference_unit(referenceUnit) 첫번째 완성후 넘어갈것
 
-hx.reset()
+# 초기화 및 영점 설정
+hx1.reset()
+# hx2.reset() 첫번째 완성후 넘어갈것
 
-hx.tare()
+hx1.tare()
+# hx2.tare() 첫번째 완성후 넘어갈것
 
 print("Tare done! Add weight now...")
 
-# to use both channels, you'll need to tare them both
-#hx.tare_A()
-#hx.tare_B()
-
 while True:
     try:
-        # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
-        # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
-        # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment these three lines to see what it prints.
-        
-        # np_arr8_string = hx.get_np_arr8_string()
-        # binary_string = hx.get_binary_string()
-        # print binary_string + " " + np_arr8_string
-        
-        # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
-        val = hx.get_weight(5)
-        print(val)
+        # 첫 번째 로드셀 (엑셀)
+        val_accelerator = hx1.get_weight(5)
+        print(f"엑셀 (Accelerator) 무게: {val_accelerator} g")
 
-        # To get weight from both channels (if you have load cells hooked up 
-        # to both channel A and B), do something like this
-        #val_A = hx.get_weight_A(5)
-        #val_B = hx.get_weight_B(5)
-        #print "A: %s  B: %s" % ( val_A, val_B )
+        # 두 번째 로드셀 (브레이크)
+        #val_brake = hx2.get_weight(5)
+        # print(f"브레이크 (Brake) 무게: {val_brake} g")
 
-        hx.power_down()
-        hx.power_up()
+        hx1.power_down()
+        # hx2.power_down()
+        hx1.power_up()
+        # hx2.power_up()
         time.sleep(0.1)
 
     except (KeyboardInterrupt, SystemExit):
